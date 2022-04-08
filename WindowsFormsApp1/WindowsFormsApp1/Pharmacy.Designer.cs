463808e4c329a2c1fcae36f1ca3f1a5731953d61
Vinuri Disara <vinuridisara2020@gmail.com>
2022-04-08 15:11:32 +0530

    added a button

--- WindowsFormsApp1/WindowsFormsApp1/Pharmacy.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Pharmacy.Designer.cs
@@ -32,32 +32,58 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label1.Location = new System.Drawing.Point(320, 152);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(253, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 147);
+            this.label1.Size = new System.Drawing.Size(392, 47);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Pharmacy";
+            this.label1.Text = "Pharmacy Managment";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Pharmacy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Pharmacy";
             this.Text = "Pharmacy";
             this.ResumeLayout(false);
+
         }
 
         private System.Windows.Forms.Label label1;
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
