a99788f445a05cb5d787f1f86fdeea3a839ac267
Sakya Rasadi <sakyarasadi@gmail.com>
2022-04-19 22:02:36 +0530

    create backend for room managemenet

--- WindowsFormsApp1/WindowsFormsApp1/Rooms.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Rooms.cs
@@ -44,24 +44,26 @@ namespace WindowsFormsApp1
 
         private void guna2Button4_Click(object sender, System.EventArgs e)
         {
-            string room_num = guna2TextBox18.Text;
-            string id = guna2TextBox1.Text;
+            int room_num = int.Parse(guna2TextBox4.Text);
+            int id = int.Parse(guna2TextBox1.Text); 
             string name = guna2TextBox2.Text;
-            string contact = guna2TextBox3.Text;
+            int contact = int.Parse(guna2TextBox3.Text); 
             string gender = guna2ComboBox1.Text;
             string relationship = guna2TextBox14.Text;
-            string age = guna2TextBox15.Text;
+            int age = int.Parse(guna2TextBox15.Text); 
             string address = guna2TextBox5.Text;
-            string emgnum = guna2TextBox9.Text;
+            int  emgnum = int.Parse(guna2TextBox9.Text); 
             string bloodGroup = guna2TextBox16.Text;
-            string advance = guna2TextBox21.Text;
+            string RoomType = guna2ComboBox2.Text;
+            int Price = int.Parse(guna2TextBox18.Text); 
+            int advance = int.Parse(guna2TextBox21.Text); 
             string consultant = guna2TextBox11.Text;
             string date = guna2TextBox13.Text;
 
             //SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='D:\DATABASE Servers\Project.mdf';Integrated Security=True;Connect Timeout=30");
             SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='D:\DATABASE Servers\Project.mdf';Integrated Security=True;Connect Timeout=30");
 
-            string sql = "INSERT INTO RoomManagemenet  VALUES ('" + room_num + "', '" + id + "', '" + name + "', '" + contact + "', '" + gender + "', '" + relationship + "', '" + age + "', '" + address + "', '" + emgnum + "', '" + bloodGroup + "', '" + advance + "', '" + consultant + "', '" + date + "')";
+            string sql = "INSERT INTO RoomManagemenet  VALUES ('" + room_num + "', '" + id + "', '" + name + "', '" + contact + "', '" + gender + "', '" + relationship + "', '" + age + "', '" + address + "', '" + emgnum + "', '" + bloodGroup + "','" + RoomType + "','" + Price + "', '" + advance + "', '" + consultant + "', '" + date + "')";
 
             SqlCommand cmd = new SqlCommand(sql, conn);
 
